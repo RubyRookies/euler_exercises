d698464647f918803b470ebeca0bcb6b1eba5f87
dklounge <daviddarden11@gmail.com>
2013-04-28 15:53:58 -0700

    updates to Sunday

--- pr11_gridproduct.rb
+++ pr11_gridproduct.rb
@@ -1,13 +1,30 @@
 # def gridproduct
 # end
 
-def test_box
-  array = [[1,2,3,4],
-          [5,6,7,8], 
-          [9,10,11,12], 
-          [13,14,15,16]]
+def gridproduct
+
+  product_row, product_column = 1, 1
+
+  rows_array = [] # to store numerical array
+  data = File.readlines("pr11_source").each do |line|
+    rows_array << line.split(" ").map { |el| el.to_i }
+  end
   
-  p array[0..1][0..1]
+# rows_array.each_with_index { |row, index| row.each { |el| p el } }
+
+  rows_array.each_with_index do |row| 
+    row.each { |el| p el }
+  end
+
 end
 
-test_box
+def array_product
+  product = 1
+  test_array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+  test_array.each_with_index do |row, column|
+    p column, row
+  end
+end
+
+array_product
